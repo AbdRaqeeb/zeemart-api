065676f74f4b89d3094bc8dd970cde2cbf719825
Horlarmeday <ajaomahmud@gmail.com>
2020-09-08 22:32:19 +0100

    updating interface

--- admin-dashboard/src/Components/Pages/Home.js
+++ admin-dashboard/src/Components/Pages/Home.js
@@ -20,3 +20,5 @@ const Home = () => {
 const wrapper = "kt-grid__item kt-grid__item--fluid kt-grid kt-grid--ver kt-page";
 const body = "kt-quick-panel--right kt-demo-panel--right kt-offcanvas-panel--right kt-header--fixed kt-header-mobile--fixed kt-subheader--fixed kt-subheader--enabled kt-subheader--solid kt-aside--enabled kt-aside--fixed kt-page--loading";
 const container = "kt-grid kt-grid--hor kt-grid--root";
+
+export default Home;
