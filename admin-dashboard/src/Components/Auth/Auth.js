96208a4f60d46ff1e5ff41c8ad1533c8a845c3ed
Horlarmeday <ajaomahmud@gmail.com>
2020-09-08 23:23:33 +0100

    updated interface

--- admin-dashboard/src/Components/Auth/Auth.js
+++ admin-dashboard/src/Components/Auth/Auth.js
@@ -55,7 +55,7 @@ const Auth = (props) => {
                 <div className="kt-grid kt-grid--ver kt-grid--root">
                     <div className="kt-grid kt-grid--hor kt-grid--root kt-login kt-login--v2 kt-login--signin" id="kt_login">
                         <div className="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor"
-                             style={{backgroundImage: `url(https://res.cloudinary.com/dho9flazo/image/upload/v1599595579/dashboard/logos/bg-1_uiiwsq.jpg)`, height: '100vh'}}>
+                             style={{backgroundImage: `url(https://res.cloudinary.com/dho9flazo/image/upload/v1599595579/dashboard/logos/bg-1_uiiwsq.jpg)`, height: '100vh', overflowY: 'hidden'}}>
                             <div className="kt-grid__item kt-grid__item--fluid kt-login__wrapper">
                                 <div className="kt-login__container">
                                     <div className="kt-login__logo">
